716a93f9f967ae2ac89288e6558cef13b3bbaaaf
nqueen <nqueen@nqueen>
2022-03-17 17:32:14 +0530

    Commit

---
+++ src/Inheritance_25Feb2022/Person.java
@@ -0,0 +1,21 @@
+package Inheritance_25Feb2022;
+//Structure for hierarchical  Inheritance
+public class Person {
+	public static void main(String args[]) {
+		long adhar;
+		String name;
+		int age;
+		String city;
+		char gender;
+	}
+
+}
+class Student extends Person{
+	//variables and methods
+}
+class ClubPresident extends Person{
+	
+}
+class Topper extends Person{
+	
+}
